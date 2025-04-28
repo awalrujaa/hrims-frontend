f3a4aba66434330ba7b906c8d02a6dbbf84f1c02
Ruja Awal <rujaawal@nchl.local>
2025-04-28 17:45:28 +0545

    Frontend of HRIMS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { SideBarComponent } from "./side-bar/side-bar.component";
+import { MainBodyComponent } from "./main-body/main-body.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, SideBarComponent, MainBodyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
