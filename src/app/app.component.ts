426f7fd059b5a8740b5d7bcab620dc7e000b929f
Ruja Awal <rujaawal@nchl.local>
2025-04-29 11:33:34 +0545

    simple frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { SideBarComponent } from "./side-bar/side-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { AddDepartmentComponent } from './add-department/add-department.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainBodyComponent},
+    {path: 'add-department', component: AddDepartmentComponent},
+];
