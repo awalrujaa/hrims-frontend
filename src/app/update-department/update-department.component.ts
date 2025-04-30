95e690e3ab898f7eb0e2b743bdc726a1b0fe95b8
Ruja Awal <rujaawal@nchl.local>
2025-04-30 17:57:50 +0545

    Added UI of Update Department

--- src/app/update-department/update-department.component.ts
+++ src/app/update-department/update-department.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { DepartmentService } from '../department.service';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-update-department',
-  imports: [],
+  imports: [MatIconModule, RouterLink, RouterLinkActive, FormsModule, CommonModule],
   templateUrl: './update-department.component.html',
   styleUrl: './update-department.component.css'
 })
